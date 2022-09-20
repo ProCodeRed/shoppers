7a6b6f9b6f43a5d5693f06ba8c56c7b4a8aa97ca
ProCodeRed <yadav.vipinkumar.com>
2022-09-21 02:14:29 +0530

    added jwt token

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import {notFound, errorHandler} from './middleware/middleware.js'
 
 dotenv.config();
@@ -10,11 +11,15 @@ const mode = process.env.NODE_ENV;
 const app = express();
 connectDB();
 
+
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 
 // creating 404 error middleware
